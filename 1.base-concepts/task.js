a964580f5dc1183c1eddee7ea6cfa46756d5265f
Dmitrii Bulatov <bulati484@gmail.com>
2022-10-30 12:13:14 +0300

    add corrections to task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,15 +3,14 @@
 function solveEquation(a, b, c) {
   let arr = [];
 
-
   // код для задачи №1 писать здесь
 
   let d = Math.pow(b, 2) - 4 * a * c;
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+
+  if (d === 0) {
     arr[0] = - b / (2 * a);
-  } else {
+  } 
+  if (d > 0) {
     arr[0] = (- b + Math.sqrt(d)) / (2 * a);
     arr[1] = (- b - Math.sqrt(d)) / (2 * a);
   }
@@ -24,13 +23,13 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   // код для задачи №2 писать здесь
 
-  let now = new Date();
-  let n = Math.round((date.getTime() - now.getTime()) /  (1000 * 3600 * 24) / ((30436875 / 1000000)));
-  let P = (percent / 12) / 100;
-  let S = amount - contribution;
-  let monthlyPayment = S * (P + (P / (Math.pow((1 + P), n) - 1)));
-  let totalAmountAll = monthlyPayment * n;
-  totalAmount = Math.floor(totalAmountAll) + ((Math.floor((totalAmountAll - Math.floor(totalAmountAll)) * 100)) / 100);
+  const now = new Date();
+  const n = Math.round((date.getTime() - now.getTime()) /  (1000 * 3600 * 24) / ((30436875 / 1000000)));
+  const P = (percent / 12) / 100;
+  const S = amount - contribution;
+  const monthlyPayment = S * (P + (P / (Math.pow((1 + P), n) - 1)));
+  totalAmount = Math.round((monthlyPayment * n) * 100) / 100;
+
   if (Object.is(totalAmount, NaN)) totalAmount = 'Введите срок ипотеки';
 
   return totalAmount;
