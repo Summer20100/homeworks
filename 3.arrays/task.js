7c4971021467e55c78e8095eab7eee7e5f16c70e
Dmitrii Bulatov <bulati484@gmail.com>
2022-11-02 19:52:14 +0300

    updated task.js, detited file e

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,21 +3,21 @@ function compareArrays(arr1, arr2) {
   
   if (arr1.length >= arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
-      result.push( (arr1[i] == arr2[i]) ?  true : false );
+      result.push((arr1[i] == arr2[i]) ?  true : false);
     }
   } else {
     for (let i = 0; i < arr2.length; i++) {
-      result.push( (arr2[i] == arr1[i]) ?  true : false );
+      result.push((arr2[i] == arr1[i]) ?  true : false);
     }
   }
   
- return result.every(element => element == 1 );
+  return result.every(element => element == true);
 }
 
 function advancedFilter(arr) {
   let resultArr = [];
   
   resultArr = arr.filter(el => el > 0 ).filter(el => el % 3 == 0 ).map(el => el * 10 );
-  
+
   return resultArr;
 }
