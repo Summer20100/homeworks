f0fda6c64b644ea09e55bd946c70d880e2455917
Dmitrii Bulatov <bulati484@gmail.com>
2022-11-03 20:04:49 +0300

    final version

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,23 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result = [];
-  
-  if (arr1.length >= arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      result.push((arr1[i] == arr2[i]) ?  true : false);
-    }
-  } else {
-    for (let i = 0; i < arr2.length; i++) {
-      result.push((arr2[i] == arr1[i]) ?  true : false);
-    }
-  }
-  
-  return result.every(element => element == true);
+
+  return (arr1.length === arr2.length) && arr1.every((val, ind) => val === arr2[ind]);
+
 }
 
 function advancedFilter(arr) {
-  let resultArr = [];
   
-  resultArr = arr.filter(el => el > 0 ).filter(el => el % 3 == 0 ).map(el => el * 10 );
+  return arr.filter(el => el > 0 ).filter(el => el % 3 == 0 ).map(el => el * 10 );
 
-  return resultArr;
 }
