f7d2dfcc84805398241ad532cce7212f49ba0888
Dmitrii Bulatov <bulati484@gmail.com>
2022-11-05 20:19:41 +0300

    update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(marks) {
+  if (this.marks === undefined) { 
+    this.marks = [marks];
+  } else {
+    this.marks.push(marks);
+  }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  if (this.marks === undefined) { 
+    this.marks = [...marks];
+  } else {
+    this.marks.push(...marks);
+  }
+}
+
+Student.prototype.getAverage = function() {
+  if (this.marks === undefined) {
+    return 0;
+  }
+  
+  let sum = 0;
+  this.marks.forEach((val) => {
+  sum += parseInt(val);
+  avg = sum / this.marks.length;
+  })
+  return avg;
+
+  //return this.marks.reduce((inc, val) => (inc + val/this.marks.length), 0);
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
